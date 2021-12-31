2a061583a59ce7dc2c6418ae9a823ce05baccbf6
Rodolfo <rndcsjj@gmail.com>
2021-12-31 11:46:43 -0300

    Aulas do dia 31/12/21

--- Alura/array/forache.php
+++ Alura/array/forache.php
@@ -1,17 +1,36 @@
 <?php
 $contaCorrente = [
-    123456789 => [
+    '123.400.567-89' => [
         "titular" => "vinicius",
         "saldo" => 1000
     ],
-    1234565689 => [
+    '123.456.560-89' => [
         "titular" => "Carlos",
-        "saldo" => 1000
+        "saldo" => 400
     ]
 ];
-foreach($contaCorrente as $cpf =>$conta) {
-    echo "Nome: {$conta['titular']} <br>
-    conta: {$conta['saldo']} <hr>" . "<br>"; //Pegando o nome dos titulares
- 
+
+function exibirMensagem($mensagem) {
+    echo " $mensagem <br>";
 }
+
+function Saque($conta, $ValorSaque) {
+    if($ValorSaque > $conta["saldo"]) {
+        exibirMensagem("Você não pode sacar");
+    } else {
+        $conta["saldo"] -= $ValorSaque;
+    }
+    return $conta;
+}
+$contaCorrente['123.456.560-89'] = Saque($contaCorrente['123.456.560-89'], 500);
+$contaCorrente['123.400.567-89'] = Saque($contaCorrente['123.400.567-89'], 500);
+
+
+// echo '<br>' . $contaCorrente['123456789']['saldo'] -=500 ;
+
+
+foreach($contaCorrente as $cpf =>$conta) {
+    ExibirMensagem("Nome: {$conta['titular']} <br>conta: {$conta['saldo']} <hr>");
+}
+
 ?>
