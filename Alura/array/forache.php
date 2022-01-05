f35f3b2499fe40c255a1760a5216fad1c9daf0ab
Rodolfo <rndcsjj@gmail.com>
2022-01-05 16:56:00 -0300

    Aulas sobre função de hoje

--- Alura/array/forache.php
+++ Alura/array/forache.php
@@ -10,11 +10,13 @@ $contaCorrente = [
     ]
 ];
 
-function exibirMensagem($mensagem) {
+function exibirMensagem($mensagem) 
+{
     echo " $mensagem <br>";
 }
 
-function Saque($conta, $ValorSaque) {
+function Saque($conta, float $ValorSaque) 
+{
     if($ValorSaque > $conta["saldo"]) {
         exibirMensagem("Você não pode sacar");
     } else {
@@ -22,15 +24,32 @@ function Saque($conta, $ValorSaque) {
     }
     return $conta;
 }
-$contaCorrente['123.456.560-89'] = Saque($contaCorrente['123.456.560-89'], 500);
-$contaCorrente['123.400.567-89'] = Saque($contaCorrente['123.400.567-89'], 500);
+
+$contaCorrente['123.400.567-89'] = Saque($contaCorrente['123.400.567-89'], 500.00);
+$contaCorrente['123.456.560-89'] = Saque($contaCorrente['123.456.560-89'], 500.00);
+//-----------------------------------------------------------------------------------
+
+function depositar(array $conta, float $valorDeposito) :array 
+{
+    if($valorDeposito > 0) {
+        $conta['saldo'] += $valorDeposito;
+    } else {
+        exibirMensagem("Valor precisa ser maior que 0");
+    }
+    return $conta;
+}
+
+$contaCorrente['123.400.567-89'] = depositar($contaCorrente['123.400.567-89'], 500.45);
+$contaCorrente['123.456.560-89'] = depositar($contaCorrente['123.456.560-89'], 200.00);
 
 
+foreach($contaCorrente as $cpf =>$conta) 
+{
+    ExibirMensagem("Nome: {$conta['titular']} <br>
+    conta: {$conta['saldo']} <hr>");
+}
 // echo '<br>' . $contaCorrente['123456789']['saldo'] -=500 ;
 
 
-foreach($contaCorrente as $cpf =>$conta) {
-    ExibirMensagem("Nome: {$conta['titular']} <br>conta: {$conta['saldo']} <hr>");
-}
 
 ?>
